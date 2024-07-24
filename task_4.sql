caa7a66c872af804f289ae3ee838f649da50755d
NABILATMANI <atmaninabile@gmail.com>
2024-07-24 14:24:51 +0100

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Query to get full description of the books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+  AND TABLE_SCHEMA = 'alx_book_store';
